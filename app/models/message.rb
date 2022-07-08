a1f0f28efbb9d0a5ff6027b5ab5c6cd35edf7377
tunnelkowai <ueda1410013@yahoo.co.jp>
2022-07-08 17:22:52 +0900

    メッセージ送信機能の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
-class Mssage < ApplicationRecord
+class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
 end
--- db/migrate/20220708080049_create_messages.rb
+++ db/migrate/20220708080049_create_messages.rb
@@ -1,6 +1,6 @@
-class CreateMssages < ActiveRecord::Migration[6.0]
+class CreateMessages < ActiveRecord::Migration[6.0]
   def change
-    create_table :mssages do |t|
+    create_table :messages do |t|
       t.string :content
       t.references :user, null: false, foreign_key: true
       t.references :room, null: false, foreign_key: true
